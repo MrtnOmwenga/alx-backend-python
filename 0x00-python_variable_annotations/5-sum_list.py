b095bfb33e7685491b8cc9e2a076ec0123139010
MrtnOmwenga <martin36449@gmail.com>
2023-07-06 19:39:04 +0000

    Basic annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Adds numbers in a list
+"""
+
+
+def sum_list(input_list: List[float])-> float:
+    """
+    Returns float
+    """
+
+    return sum(input_list)
