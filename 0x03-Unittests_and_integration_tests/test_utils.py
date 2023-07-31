d56229c1c05dc7062afdf058021390ebd79ff41a
MrtnOmwenga <martin36449@gmail.com>
2023-07-31 18:53:17 +0000

    Implemented TestAccessNestedMap.test_access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,22 @@
 Unit tests
 """
 import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+from typing import Dict, Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Contains tests for utils.access_nested_map """
-    
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self,
+                               nested_map: Dict,
+                               path: Tuple[str],
+                               expected: Union[int, Dict]):
+        """ Test function for access_nested_map """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
